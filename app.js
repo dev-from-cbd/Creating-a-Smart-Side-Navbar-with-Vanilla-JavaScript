e4b691d03b18dce1e52578bf8a2eb275c03fe357
dev-from-cbd <veryfatwombat@gmail.com>
2021-07-31 10:16:44 +1000

    js

---
+++ app.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+  console.log(sidebar.classList);
+  if(sidebar.classList.contains)
+});
