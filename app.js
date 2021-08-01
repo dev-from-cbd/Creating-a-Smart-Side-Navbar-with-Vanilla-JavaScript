43013900e75c53c09aee040e9c3a746da9ffa562
dev-from-cbd <veryfatwombat@gmail.com>
2021-08-01 17:35:00 +1000

    js

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
 toggleBtn.addEventListener("click", function () {
-  console.log(sidebar.classList);
-  if(sidebar.classList.contains)
+  // if (sidebar.classList.contains("show-sidebar")) {
+  //   sidebar.classList.remove("show-sidebar");
+  // } else {
+  //   sidebar.classList.add("show-sidebar");
+  // }
+  sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
 });
